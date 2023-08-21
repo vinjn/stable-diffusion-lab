2fe1f8ec42d8229adb35e312eaf16144081fc467
vincentz <vincentz@nvidia.com>
2023-08-20 23:52:25 -0700

    1111 - add more extensions, models and loras

--- setup-1111.bat
+++ setup-1111.bat
@@ -8,9 +8,23 @@ git clone https://github.com/Mikubill/sd-webui-controlnet.git
 git clone https://github.com/butaixianran/Stable-Diffusion-Webui-Civitai-Helper.git
 git clone https://github.com/ArtVentureX/sd-webui-agent-scheduler.git
 git clone https://github.com/kohya-ss/sd-webui-additional-networks.git
+git clone https://github.com/zanllp/sd-webui-infinite-image-browsing.git
+git clone https://github.com/some9000/StylePile.git
 popd
 
-wget https://huggingface.co/XpucT/Deliberate/resolve/main/Deliberate_v2.safetensors -N -P models/Stable-diffusion/
+@REM base models
+call wget https://huggingface.co/XpucT/Deliberate/resolve/main/Deliberate_v2.safetensors -N -P models/Stable-diffusion/
+call wget https://huggingface.co/SG161222/Realistic_Vision_V5.1_noVAE/resolve/main/Realistic_Vision_V5.1_fp16-no-ema.safetensors -N -P models/Stable-diffusion/
+
+@REM LORA
+call wget https://huggingface.co/digiplay/LORA/resolve/main/add_brightness.safetensors -N -P models/Lora/
+call wget https://huggingface.co/digiplay/LORA/resolve/main/add_detail.safetensors -N -P models/Lora/
+call wget https://huggingface.co/digiplay/LORA/resolve/main/add_saturation.safetensors -N -P models/Lora/
+call wget https://huggingface.co/digiplay/LORA/resolve/main/add_sharpness.safetensors -N -P models/Lora/
+call wget https://huggingface.co/digiplay/LORA/resolve/main/epi_noiseoffset2.safetensors -N -P models/Lora/
+call wget https://huggingface.co/JCTN/lora/resolve/main/LowRA.safetensors -N -P models/Lora
+call wget https://huggingface.co/JCTN/lora/resolve/main/lit.safetensors -N -P models/Lora
+call wget https://huggingface.co/JCTN/lora/resolve/main/COOLKIDS_MERGE_V2.5.safetensors -N -P models/Lora
 
 set COMMANDLINE_ARGS=--xformers
 webui.bat
