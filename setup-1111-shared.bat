0b2487bc8f69ad2fb7aae7251ab68f8ee45f9e9a
vinjn <vinjn.z@gmail.com>
2023-12-30 19:39:08 -0800

    update bat

--- setup-1111-shared.bat
+++ setup-1111-shared.bat
@@ -5,10 +5,51 @@ git pull
 
 pushd extensions
 git clone https://github.com/Mikubill/sd-webui-controlnet.git
+pushd sd-webui-controlnet
+git pull
+
+popd
 git clone https://github.com/butaixianran/Stable-Diffusion-Webui-Civitai-Helper.git
+pushd Stable-Diffusion-Webui-Civitai-Helper
+git pull
+popd
+
 git clone https://github.com/ArtVentureX/sd-webui-agent-scheduler.git
+pushd sd-webui-agent-scheduler
+git pull
+popd
+
 git clone https://github.com/zanllp/sd-webui-infinite-image-browsing.git
+pushd sd-webui-infinite-image-browsing
+git pull
+popd
+
+git clone https://github.com/alemelis/sd-webui-ar.git
+pushd sd-webui-ar
+git pull
+popd
+
+git clone https://github.com/camenduru/stable-diffusion-webui-artists-to-study.git
+pushd stable-diffusion-webui-artists-to-study
+git pull
+popd
+
+git clone https://github.com/Bing-su/adetailer.git
+pushd adetailer
+git pull
+popd
+
+git clone https://github.com/aigc-apps/sd-webui-EasyPhoto.git
+pushd sd-webui-EasyPhoto
+git pull
+popd
+
 git clone https://github.com/civitai/sd_civitai_extension.git
+pushd sd_civitai_extension
+git pull
+
+popd
+
 popd
 
 wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1p_sd15_depth.pth -N -P extensions/sd-webui-controlnet/models
@@ -31,5 +72,6 @@ wget https://huggingface.co/Neburozakusu/civitai_deposit/resolve/main/bad-hands-
 wget https://huggingface.co/Neburozakusu/civitai_deposit/resolve/main/NG_DeepNegative_V1_75T.pt -N -P embeddings/
 wget https://huggingface.co/datasets/Nerfgun3/bad_prompt/resolve/main/bad_prompt_version2.pt -N -P embeddings/
 wget https://huggingface.co/gemasai/verybadimagenegative_v1.3/resolve/main/verybadimagenegative_v1.3.pt -N -P embeddings/
+wget https://huggingface.co/AsciiP/badhandv4/resolve/main/badhandv4.pt -N -P embeddings/
 
 popd
